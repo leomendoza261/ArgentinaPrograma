edd1aff446af2ffded6ab91a1d1b5cb600c6eda3
Leo Mendoza <77596@sistemas.frc.utn.edu.ar>
2023-05-27 10:37:22 -0300

    practico 0 finalizado

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,9 +1,8 @@
-//A 
-let fahrenheitA = 50
-let celsiusA = (fahrenheitA - 32) * (5/9)
-console.log("A - Grados Fahrenheit: ", fahrenheitA, "=> Celsius: ", celsiusA)
-
-//B 
-let fahrenheitB = 70
-let celsiusB = (fahrenheitB - 32) * (5/9)
-console.log("A - Grados Fahrenheit: ", fahrenheitB, "=> Celsius: ", celsiusB)
+function convertirGrados(fahrenheit) {
+    let celsius = (fahrenheit - 32) * (5 / 9);
+    return celsius;
+  }
+  
+  let fahrenheit = 70;
+  let celsius = convertirGrados(fahrenheit);
+  console.log(fahrenheit + " grados Fahrenheit => " + celsius + " grados Celsius.");
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,20 +1,14 @@
-//A 
-let x = 5
-let y = 7
-let z = 9
-let promedio = (x*y*z)/3
-console.log(promedio)
+function calcularPromedio(num1, num2, num3) {
+    let suma = num1 + num2 + num3;
+    let promedio = suma / 3;
+    return promedio;
+  }
 
-//A 
-let xb = -2
-let yb = -3
-let zb = -8
-let promediob = (xb*yb*zb)/3
-console.log(promediob)
+let promedioA = calcularPromedio(5,7,9)
+console.log(promedioA)
 
-//C
-let xc = -1.2
-let yc= 5.3
-let zc = -3.3
-let promedioc = (xc*yc*zc)/3
-console.log(promedioc)
+let promedioB = calcularPromedio(-2,-3,-8)
+console.log(promedioB)
+
+let promedioC = calcularPromedio(-1.2,5.3,-3.3)
+console.log(promedioC)
