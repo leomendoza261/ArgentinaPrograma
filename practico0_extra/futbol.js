f7eb51624ccd9bd30386252b1d6a3dd98a39c7ba
Leo Mendoza <77596@sistemas.frc.utn.edu.ar>
2023-06-13 19:50:07 -0300

    correccion de nombre de variables

--- practico0_extra/futbol.js
+++ practico0_extra/futbol.js
@@ -2,19 +2,19 @@ const jugador1 = {
     nombre: "Dibu Martinez",
     posicion: "arco",
     edad: 27
-  };
-  
-  const jugador2 = {
+};
+
+const jugador2 = {
     nombre: "Cuti Romero",
     posicion: "defensa",
     edad: 23
-  };
+};
 
-  const jugador3 = {
+const jugador3 = {
     nombre: "Lionel Messi",
     posicion: "delantero",
     edad: 35
-  };
+};
 //aqui deberia crear mas jugadores
 
 const equipo1 = [jugador1, jugador2, jugador3] //solo puse 3 pero asi se utiliza un arreglo que represente un quipo
--- practico1/cita.js
+++ practico1/cita.js
@@ -1,8 +1,11 @@
-const cita = "tres tristes tigres comen trigo";
-const substring = "tigres comen trigo";
+let cita = "Tres tristes tigres comen trigo en un trigal";
+let substring = "tigres comen trigo";
 
-const tamañoCita = cita.length;
-console.log("el tamañao de la cita es:", tamañoCita)
+let tamañoDeCita = cita.length;
+console.log("el tamañao de la cita es:", tamañoDeCita)
 
-const indice = cita.indexOf(substring);
+let indice = cita.indexOf(substring);
 console.log("El indice del substring es:", indice)
+
+let citaRevisada = cita.slice(0, indice) + substring; 
+console.log(citaRevisada)
